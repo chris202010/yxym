f8c3d1850e4b2202caf0bbbdcb5d200151ad0218
chris202010 <67861894+chris202010@users.noreply.github.com>
2025-10-02 08:18:52 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -19,7 +19,7 @@ urls = [
     'https://addressesapi.090227.xyz/ip.164746.xyz',
     'https://ipdb.api.030101.xyz/?type=cfv4;proxy',
     'https://ipdb.api.030101.xyz/?type=bestcf&country=true',
-    'https://ipdb.api.030101.xyz/?type=bestproxy&country=true',
+    # 'https://ipdb.api.030101.xyz/?type=bestproxy&country=true',
     # 'https://www.wetest.vip/page/edgeone/address_v4.html',
     # 'https://www.wetest.vip/page/cloudfront/address_v4.html',
     # 'https://www.wetest.vip/page/cloudflare/address_v4.html'
